bae286433c7a838178511177a98e510448d9fb2d
KalpithaG <KalpithaG>
2025-03-25 08:27:52 +0000

    Day 11

--- Day10/create-pipeline-from-ecom-data/ecom-DLT-pipeline-notebook.sql
+++ Day10/create-pipeline-from-ecom-data/ecom-DLT-pipeline-notebook.sql
@@ -72,3 +72,46 @@ COLUMNS * EXCEPT
   (operation, sequenceNum,_rescued_data)
 STORED AS
   SCD TYPE 2;
+
+-- COMMAND ----------
+
+create or refresh materialized view customers_active as
+select * from cgi_dev.kalpitha_bronze.customers_silver where __END_AT is null
+
+-- COMMAND ----------
+
+create or replace materialized view cgi_dev.kalpitha_gold.sales_details as 
+(SELECT 
+    o.order_id,
+    o.order_date,
+    o.customer_id,
+    c.customer_name,
+    c.customer_email,
+    c.customer_city,
+    o.product_id,
+    p.product_name,
+    p.product_category,
+    p.product_price,
+    o.quantity,
+    o.discount_amount,
+    o.total_amount
+FROM cgi_dev.kalpitha_bronze.sales_cleaned o
+JOIN cgi_dev.kalpitha_bronze.product_silver p
+  ON o.product_id = p.product_id
+JOIN (
+      SELECT DISTINCT customer_id, customer_name, customer_email, customer_city 
+      FROM cgi_dev.kalpitha_bronze.customers_active
+     ) c
+  ON o.customer_id = c.customer_id)
+
+-- COMMAND ----------
+
+create or replace live view customer_revenue as
+SELECT 
+    customer_id, 
+    customer_name, 
+    round(SUM(total_amount)) AS total_revenue
+FROM cgi_dev.kalpitha_gold.sales_details
+GROUP BY customer_id, customer_name
+ORDER BY total_revenue DESC
+LIMIT 3;
